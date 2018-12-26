690dad5f3804834340c9656d73028d169c1008bc
liudongxu <562430258@qq.com>
2018-12-26 15:08:08 +0800

    项目搭建

---
+++ src/main/java/io/feikuai/AutoTable2beanApplication.java
@@ -0,0 +1,14 @@
+package io.feikuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTable2beanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTable2beanApplication.class, args);
+	}
+
+}
+
